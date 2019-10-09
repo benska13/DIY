921443e63c465467ba140eeb508cbbbffaa2c0c3
Benjamin Odland Skare <benjamin.skare@outlook.com>
2019-10-09 20:30:33 +0200

    1

---
+++ TempControl/TempControl/TempControl/TempControl.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TempControl.ino
+ Created:	10/9/2019 8:26:03 PM
+ Author:	47974
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
